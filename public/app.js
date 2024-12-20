988ce822f6f51fd7b436109f3d3abc2e1984de2f
Kevin Guzman <kevincastrog28@gmail.com>
2024-12-20 14:16:41 -0500

    Changing port

--- public/app.js
+++ public/app.js
@@ -48,6 +48,7 @@ app.get('/get-similar', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
 });
