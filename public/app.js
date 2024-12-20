fa07ba5302c5dfaae796cbf7a7bb6a136714d85a
Kevin Guzman <kevincastrog28@gmail.com>
2024-12-20 14:40:33 -0500

    Added centering CSS styles

--- public/app.js
+++ public/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));
+
+
 // Route to render the index
 app.get('', (req, res) => {
     res.render('index', {
